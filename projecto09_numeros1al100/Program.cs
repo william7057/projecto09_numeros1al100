ede3c6333601bcaff96213f42175e1e7aa83de39
william <factzure@hotmail.com>
2021-09-08 12:38:40 -0500

    Agregar archivos de proyecto.

---
+++ projecto09_numeros1al100/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace projecto09_numeros1al100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = 1;
+            while (x<=100)
+            {
+                //Console.WriteLine(x);
+                Console.Write(x + " ");
+                x = x + 1;
+            }
+            Console.ReadKey();
+        }
+    }
+}
